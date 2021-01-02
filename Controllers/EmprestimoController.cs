8b76db3a54bd8262ab27afa2d59007af0cf037ee
Glauco <glauco.marengo@hotmail.com>
2021-01-02 12:19:23 -0300

    Todas as páginas precisam de login

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -9,8 +9,11 @@ namespace Biblioteca.Controllers
     
     public class EmprestimoController : Controller
     {
+        
         public IActionResult Cadastro()
         {
+            Autenticacao.CheckLogin(this);
+
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
@@ -22,6 +25,7 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
+            
             EmprestimoService emprestimoService = new EmprestimoService();
             
             if(viewModel.Emprestimo.Id == 0)
@@ -37,6 +41,8 @@ namespace Biblioteca.Controllers
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
+            Autenticacao.CheckLogin(this);
+
             FiltrosEmprestimos objFiltro = null;
             if(!string.IsNullOrEmpty(filtro))
             {
@@ -50,6 +56,8 @@ namespace Biblioteca.Controllers
 
         public IActionResult Edicao(int id)
         {
+            Autenticacao.CheckLogin(this);
+
             LivroService livroService = new LivroService();
             EmprestimoService em = new EmprestimoService();
             Emprestimo e = em.ObterPorId(id);
